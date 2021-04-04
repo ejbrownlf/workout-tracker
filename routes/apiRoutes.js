16e1b16c9f13296617eb3d098e3dd2979a1bd3cd
ejbrownlf <ejbrownlf@gmail.com>
2021-04-04 14:26:18 -0500

    fixed html routes and started api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const mongoose = require('mongoose');
+const { Workout } = require('../models');
 
 module.exports = () => {
-    router.get('/workouts', );
+    router.get('/workouts', (req, res) => {
+        Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.status(400).json(err)
+            })
+    });
     router.post('/workouts');
     router.put('/workouts',);
     router.get('/workouts/range');
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const path = require('path');
 
-module.exports = () => {
-    router.get('/exercise', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    });
-}
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+
+module.exports = router;
